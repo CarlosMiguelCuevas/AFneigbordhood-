ed26ed52d00b939a6d13a700027f954453970819
Carlos Cuevas <carlos.cuevas.ochoa@gmail.com>
2025-07-26 14:42:14 -0700

    Initial Commit

---
+++ AFneigbordhood/AFneigbordhoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFneigbordhoodApp.swift
+//  AFneigbordhood
+//
+//  Created by M00306 on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct AFneigbordhoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
