09e18d2f20810273ce471095a59f1f59062dc3ad
Carlos Cuevas <carlos.cuevas.ochoa@gmail.com>
2025-08-15 22:02:16 -0700

    format code

--- AFneigbordhood/Home/ui/Home.swift
+++ AFneigbordhood/Home/ui/Home.swift
@@ -11,7 +11,7 @@ struct Home: View {
 
     @Environment(HomeManager.self) private var homeManager
     @State private var homeViewModel = HomeViewModel()
-    
+
     var body: some View {
         ZStack {
             Color.red.opacity(0.1)
@@ -22,41 +22,51 @@ struct Home: View {
                     AfSectionHeader(title: "Upcoming Events")
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack(spacing: 16) {
-                            ForEach(homeViewModel.upcomingEvents ?? []) { event in
-                                AfFeaturedCard(image: Image(systemName: "photo"),
-                                               title: event.title,
-                                               description: event.description)
+                            ForEach(homeViewModel.upcomingEvents ?? []) {
+                                event in
+                                AfFeaturedCard(
+                                    image: Image(systemName: "photo"),
+                                    title: event.title,
+                                    description: event.description
+                                )
                             }
                         }
                         .padding(.horizontal)
                     }
-                    
+
                     // Important Messages Section
                     AfSectionHeader(title: "Important Messages")
-                    
+
                     VStack(spacing: 16) {
-                        ForEach(homeViewModel.importantMessages ?? []) { message in
+                        ForEach(homeViewModel.importantMessages ?? []) {
+                            message in
                             AfListItem(
                                 image: Image(systemName: "house"),
-                                title: message.title, description: message.description)
+                                title: message.title,
+                                description: message.description
+                            )
                         }
                     }
                     .padding(.horizontal)
-                    
-                    
-                    
+
                     // Committee Members Section
                     AfSectionHeader(title: "Committee Members")
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack(spacing: 16) {
-                            ForEach(homeViewModel.committeeMebers ?? []) { member in
-                                AfFeaturedCard(image: Image(systemName: "person.crop.circle"), title: member.name,
-                                               description: member.role.rawValue)
+                            ForEach(homeViewModel.committeeMebers ?? []) {
+                                member in
+                                AfFeaturedCard(
+                                    image: Image(
+                                        systemName: "person.crop.circle"
+                                    ),
+                                    title: member.name,
+                                    description: member.role.rawValue
+                                )
                             }
                         }
                         .padding(.horizontal)
                     }
-                    
+
                     // Contact Administration Section
                     AfSectionHeader(title: "Contact Administration")
                     Button(action: {
@@ -66,11 +76,14 @@ struct Home: View {
                     }) {
                         AfListItem(
                             image: Image(systemName: "phone"),
-                            title: homeViewModel.administationContactInformation?.adminName ?? "",
-                            description: "Contact us thourgh Whatsapp \(homeViewModel.administationContactInformation?.phoneNumber ?? "")"
+                            title: homeViewModel
+                                .administationContactInformation?.adminName
+                                ?? "",
+                            description:
+                                "Contact us thourgh Whatsapp \(homeViewModel.administationContactInformation?.phoneNumber ?? "")"
                         )
                     }
-                    .buttonStyle(PlainButtonStyle()) 
+                    .buttonStyle(PlainButtonStyle())
                     .padding(.horizontal)
                 }
                 .padding(.vertical)
@@ -80,12 +93,21 @@ struct Home: View {
             await homeViewModel.loadUpcomingEvents(homeManager: homeManager)
             await homeViewModel.loadImportantMessages(homeManager: homeManager)
             await homeViewModel.loadCommitteeMebers(homeManager: homeManager)
-            await homeViewModel.loadAdministationContactInformation(homeManager: homeManager)
+            await homeViewModel.loadAdministationContactInformation(
+                homeManager: homeManager
+            )
         }
     }
 }
 
-
 #Preview {
     Home()
+        .environment(
+            HomeManager(
+                homeEventsService: MockedHomeEventsService(),
+                homeMessagesService: MockedHomeMessagesService(),
+                homeCommitteeService: MockedHomeCommitteeMembersService(),
+                homeAdminContactService: MockedHomeAdminContactService()
+            )
+        )
 }
